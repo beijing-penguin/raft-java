21a3d850900b72d81761c9663745d8b64dd7a3d4
echo企鹅 <429544557@qq.com>
2017-12-11 17:51:30 +0800

    更新...

--- src/main/java/org/dc/penguin/core/utils/NodeUtils.java
+++ src/main/java/org/dc/penguin/core/utils/NodeUtils.java
@@ -119,7 +119,7 @@ public class NodeUtils {
 				raf.read(bytes);
 				lastLine = new String(bytes, "utf-8"); 
 			}
-			
+
 			if(StringUtils.isNotEmpty(lastLine)) {
 				Message message = JSON.parseObject(lastLine,Message.class);
 				nodeInfo.setDataIndex(new AtomicInteger(Integer.parseInt(message.getLeaderKey().split(":")[4])));
@@ -135,36 +135,7 @@ public class NodeUtils {
 		}
 	}
 	public static void main(String[] args) throws Exception {
-		RandomAccessFile raf = null;
-		try {
-			raf = new RandomAccessFile("/data/raft/dataLog8881", "r");
-			long len = raf.length();
-			String lastLine = null;
-			if (len != 0L) {
-				long pos = len - 1;
-				while (pos > 0) {
-					pos--;
-					raf.seek(pos);
-					if (raf.readByte() == '\n') {
-						break;
-					}
-				}
-				if (pos == 0) {  
-					raf.seek(0);  
-				}  
-				byte[] bytes = new byte[(int) (len - pos)];
-				raf.read(bytes);
-				lastLine = new String(bytes, "utf-8"); 
-			}
-			System.out.println(lastLine);
-			System.out.println(JSON.parseObject(lastLine, Message.class));
-		}catch (Exception e) {
-			throw e;
-		}finally {
-			if(raf!=null) {
-				raf.close();
-			}
-		}
+		
 	}
 	public static void logSync(NodeInfo mynodeInfo) {
 		try {
@@ -173,71 +144,76 @@ public class NodeUtils {
 					new Thread(new Runnable() {
 						@Override
 						public void run() {
+							
 							SocketPool pool = SocketCilentUtils.getSocketPool(nodeInfo.getHost(), nodeInfo.getElectionServerPort());
 							SocketConnection conn = null;
+							RandomAccessFile rf = null;
 							try {
+								List<Message> syncList = new ArrayList<Message>(); 
+								
 								conn = pool.getSocketConnection();
 								Message msg = new Message();
 								msg.setMsgCode(MsgType.GET_DATA_POS);
 								String rt_str = conn.sendMessage(msg.toJSONString());
-								int node_term = Integer.parseInt(rt_str.split(":")[0]);
-								int node_dataIndex = Integer.parseInt(rt_str.split(":")[1]);
-
-								List<Message> syncList = new ArrayList<Message>(); 
-								RandomAccessFile rf = null;
-								try {
-									rf = new RandomAccessFile(NodeConfigInfo.dataLogDir, "r");
-									long len = rf.length();
-									long start = rf.getFilePointer();
-									long nextend = start + len - 1;
-									String line = null;
-									rf.seek(nextend);
-									int c = -1;
-									while (nextend > start) {
-										c = rf.read();
-										if (c == '\n' || c == '\r') {
-											line = new String(rf.readLine().getBytes("ISO-8859-1"), "utf-8");
-											nextend--;
+								int nodeOther_term = Integer.parseInt(rt_str.split(":")[0]);
+								int nodeOther_dataIndex = Integer.parseInt(rt_str.split(":")[1]);
+								
+								rf = new RandomAccessFile(NodeConfigInfo.dataLogDir, "r");
+								long len = rf.length();
+								long start = rf.getFilePointer();
+								long nextend = start + len - 1;
+								rf.seek(nextend);
+								int c = -1;
+								String line = null;
+								boolean isOut = false;
+								while (nextend > start) {
+									c = rf.read();
+									if (c == '\n' || c == '\r') {
+										line = rf.readLine();
+										if (line != null) {
+											line = new String(line.getBytes("ISO-8859-1"), "utf-8");
+											isOut = true;
 										}
 										nextend--;
-										rf.seek(nextend);
-										if (nextend == 0) {// 当文件指针退至文件开始处，输出第一行
-											// System.out.println(rf.readLine());
-											line = new String(rf.readLine().getBytes("ISO-8859-1"), "utf-8");
-										}
-
+									}
+									nextend--;
+									rf.seek(nextend);
+									if (nextend == 0) {// 当文件指针退至文件开始处，输出第一行
+										line = new String(rf.readLine().getBytes("ISO-8859-1"), "utf-8");
+										isOut = true;
+									}
+									
+									if(isOut && StringUtils.isNoneBlank(line)) {
 										Message data_msg = JSON.parseObject(line, Message.class);
-										System.out.println("data_msg"+data_msg);
+										System.out.println("data_msg="+data_msg);
 										int my_term = Integer.parseInt(data_msg.getLeaderKey().split(":")[3]);
 										int my_dataIndex = Integer.parseInt(data_msg.getLeaderKey().split(":")[4]);
 
-										if(my_term>=node_term && my_dataIndex>node_dataIndex) {
+										if(my_term>=nodeOther_term && my_dataIndex>nodeOther_dataIndex) {
 											syncList.add(data_msg);
 										}else {
 											break;
 										}
 									}
-								}  catch (Exception e) {
-									LOG.error("",e);
-								} finally {
-									try {
-										if (rf != null)
-											rf.close();
-									} catch (Exception e) {
-										LOG.error("",e);
-									}
-								}
-
-								for (int i = syncList.size(); i > 0; i--) {
-									Message msg_sync = syncList.get(i-1);
-									msg_sync.setMsgCode(MsgType.LEADER_SET_DATA);
-									conn.sendMessage(msg_sync.toJSONString());
+									line = null;
+									isOut = false;
 								}
 							} catch (Exception e) {
 								LOG.error("",e);
-							}finally {
-								if(conn!=null) {
-									conn.close();
+							} finally {
+								try {
+									if (conn != null) {
+										conn.close();
+									}
+								} catch (Exception e) {
+									LOG.error("",e);
+								}
+								try {
+									if (rf != null) {
+										rf.close();
+									}
+								} catch (Exception e) {
+									LOG.error("",e);
 								}
 							}
 						}
